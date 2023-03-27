4a8439c39877a79551e3b67f25a060406f00839a
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2023-03-27 23:17:37 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
